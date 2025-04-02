b6cb865db0ccffd303a54adfa2daaed87a55908c
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2025-04-02 13:17:39 +0200

    Export install result models

--- lib/android_package_installer.dart
+++ lib/android_package_installer.dart
@@ -1,6 +1,8 @@
 import 'package:android_package_installer/src/install_result.dart';
 import 'package:android_package_installer/src/installer_platform.dart';
 
+export 'src/install_result.dart';
+
 class AndroidPackageInstaller {
   /// Installs apk file using Android Package Manager.
   /// Creates Package Installer session, then displays a dialog to confirm the installation.
